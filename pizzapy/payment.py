bfd4facae6f9da6335533e5fcda7b4df592763aa
Colin King <cbk1@rice.edu>
2020-09-26 03:17:34 -0500

    fixing payment.py

--- pizzapy/payment.py
+++ pizzapy/payment.py
@@ -35,14 +35,16 @@ class CreditCard(object):
 
         is_valid &= (5 <= len(self.zip) >= 6)
         print("valid3",is_valid)
-
-type(self):
-        patterns = {'VISA': r'^4[0-9]{12}(?:[0-9]{3})?$',
-                    'MASTERCARD': r'^5[1-5][0-9]{14}$',
-                    'AMEX': r'^3[47][0-9]{13}$',
-                    'DINERS': r'^3(?:0[0-5]|[68][0-9])[0-9]{11}$',
-                    'DISCOVER': r'^6(?:011|5[0-9]{2})[0-9]{12}$',
-                    'JCB': r'^(?:2131|1800|35\d{3})\d{11}$',
-                    'ENROUTE': r'^(?:2014|2149)\d{11}$'}
-        return next((card_type for card_type, pattern in list(patterns.items())
-                     if re.match(pattern, self.number)), '')
+        
+        return is_valid
+        
+    def find_type(self):
+            patterns = {'VISA': r'^4[0-9]{12}(?:[0-9]{3})?$',
+                        'MASTERCARD': r'^5[1-5][0-9]{14}$',
+                        'AMEX': r'^3[47][0-9]{13}$',
+                        'DINERS': r'^3(?:0[0-5]|[68][0-9])[0-9]{11}$',
+                        'DISCOVER': r'^6(?:011|5[0-9]{2})[0-9]{12}$',
+                        'JCB': r'^(?:2131|1800|35\d{3})\d{11}$',
+                        'ENROUTE': r'^(?:2014|2149)\d{11}$'}
+            return next((card_type for card_type, pattern in list(patterns.items())
+                        if re.match(pattern, self.number)), '')
